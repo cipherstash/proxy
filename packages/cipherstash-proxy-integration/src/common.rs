f9aa8c72b4a8c09fdf408142075b14163a8731ab
Dan Draper <dan@cipherstash.com>
2025-01-26 14:57:36 +1100

    Added Noop cert verifier for tests because Rustls is strict

--- packages/cipherstash-proxy-integration/src/common.rs
+++ packages/cipherstash-proxy-integration/src/common.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
 use rand::{distributions::Alphanumeric, Rng};
-use rustls::ClientConfig;
-use std::sync::Once;
+use rustls::{client::danger::ServerCertVerifier, crypto::{aws_lc_rs::default_provider}, pki_types::CertificateDer, ClientConfig};
+use std::sync::{Arc, Once};
 use tokio_postgres::{Client, NoTls};
 use tracing_subscriber::{filter::Directive, EnvFilter, FmtSubscriber};
 
@@ -64,13 +64,13 @@ pub fn connection_string(port: u16) -> String {
 }
 
 pub async fn connect_with_tls(port: u16) -> Client {
-    let tls_config = configure_client();
+    let tls_config = configure_test_client();
     let tls = tokio_postgres_rustls::MakeRustlsConnect::new(tls_config);
 
     let connection_string = connection_string(port);
     let (client, connection) = tokio_postgres::connect(&connection_string, tls)
         .await
-        .expect("ok");
+        .expect("connection to database to succeed");
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
@@ -96,15 +96,59 @@ pub async fn connect(port: u16) -> Client {
 }
 
 ///
-/// Configure the client TLS settings
-/// These are the settings for connecting to the database with TLS
-/// The client will use the system root certificates
+/// Configure the client TLS settings.
+/// These are the settings for connecting to the database with TLS.
 ///
-pub fn configure_client() -> ClientConfig {
-    let mut root_cert_store = rustls::RootCertStore::empty();
-    root_cert_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
-
+/// NOTE: This client uses a dangerous test certificate verifier that does not verify the server's certificate.
+/// 
+/// This is because the test database uses a self-signed certificate.
+pub fn configure_test_client() -> ClientConfig {
+    let verifier = DangerousTestCertVerifier::default();
     rustls::ClientConfig::builder()
-        .with_root_certificates(root_cert_store)
+        .dangerous()
+        .with_custom_certificate_verifier(Arc::new(verifier))
         .with_no_client_auth()
+}
+
+/// Dangerous test certificate "verifier" that does not actually verify the server's certificate.
+/// This **must** never be used for anything other than testing.
+#[derive(Debug, Default)]
+struct DangerousTestCertVerifier;
+
+impl ServerCertVerifier for DangerousTestCertVerifier {
+    fn verify_server_cert(
+            &self,
+            _end_entity: &CertificateDer<'_>,
+            _intermediates: &[CertificateDer<'_>],
+            _server_name: &rustls::pki_types::ServerName<'_>,
+            _ocsp_response: &[u8],
+            _now: rustls::pki_types::UnixTime,
+        ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
+        
+        Ok(rustls::client::danger::ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+            &self,
+            _message: &[u8],
+            _cert: &CertificateDer<'_>,
+            _dss: &rustls::DigitallySignedStruct,
+        ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        
+        Ok(rustls::client::danger::HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+            &self,
+            _message: &[u8],
+            _cert: &CertificateDer<'_>,
+            _dss: &rustls::DigitallySignedStruct,
+        ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        
+        Ok(rustls::client::danger::HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        default_provider().signature_verification_algorithms.supported_schemes()
+    }
 }
