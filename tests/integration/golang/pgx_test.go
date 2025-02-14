8162ada191df630901a5e32c6559dffd43b75bbd
Lindsay Holmwood <lindsay@holmwood.id.au>
2025-02-14 17:54:00 +1100

    test: test inserting data

--- tests/integration/golang/pgx_test.go
+++ tests/integration/golang/pgx_test.go
@@ -4,21 +4,49 @@ import (
 	"context"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/require"
 )
 
-func TestPgxConnect(t *testing.T) {
-	require := require.New(t)
+func setupPgxConnection(require *require.Assertions) *pgx.Conn {
 	dbURL := os.Getenv("DATABASE_URL")
 	require.NotEmpty(dbURL, "DATABASE_URL environment variable not set")
 
 	conn, err := pgx.Connect(context.Background(), dbURL)
 	require.NoError(err, "unable to connect to the database")
+	return conn
+}
+
+func TestPgxConnect(t *testing.T) {
+	require := require.New(t)
+	conn := setupPgxConnection(require)
 
 	var result int
-	err = conn.QueryRow(context.Background(), "select 1").Scan(&result)
+	err := conn.QueryRow(context.Background(), "select 1").Scan(&result)
 	require.NoError(err)
 	require.Equal(1, result)
+}
+
+func TestPgxInsert(t *testing.T) {
+	require := require.New(t)
+	conn := setupPgxConnection(require)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_, err := conn.Exec(ctx, `
+CREATE TEMPORARY TABLE t (name text not null unique);
+
+INSERT INTO t (name) VALUES
+	('Ada'),
+	('Grace'),
+	('Susan');
+`)
+	require.NoError(err)
+
+	var result string
+	err = conn.QueryRow(context.Background(), "SELECT name FROM t").Scan(&result)
+	require.NoError(err)
+	require.Equal("Ada", result)
 }
