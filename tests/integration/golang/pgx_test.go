00d8bc11809dca50f41db23432d23592516e5704
Lindsay Holmwood <lindsay@holmwood.id.au>
2025-02-15 00:29:33 +1100

    test: add golang integration test for mapping floats

--- tests/integration/golang/pgx_test.go
+++ tests/integration/golang/pgx_test.go
@@ -121,3 +121,35 @@ func TestPgxEncryptedMapInts(t *testing.T) {
 		})
 	}
 }
+
+func TestPgxEncryptedMapFloat(t *testing.T) {
+	require := require.New(t)
+	conn := setupPgxConnection(require)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	column := "encrypted_float8"
+	value := 1.5
+	insertStmt := fmt.Sprintf(`INSERT INTO encrypted (id, %s) VALUES ($1, $2)`, column)
+	selectStmt := fmt.Sprintf(`SELECT id, %s FROM encrypted WHERE id=$1`, column)
+
+	for _, mode := range modes {
+		id := rand.Int()
+		// Skip undefined behaviour of floats in simple protocol
+		if mode == pgx.QueryExecModeExec || mode == pgx.QueryExecModeSimpleProtocol {
+			continue
+		}
+		t.Run(mode.String(), func(t *testing.T) {
+			assert := assert.New(t)
+			_, err := conn.Exec(ctx, insertStmt, mode, id, value)
+			assert.NoError(err)
+
+			var rid int
+			var rv float64
+			err = conn.QueryRow(context.Background(), selectStmt, id).Scan(&rid, &rv)
+			assert.NoError(err)
+			assert.Equal(id, rid)
+			assert.Equal(value, rv)
+		})
+	}
+}
