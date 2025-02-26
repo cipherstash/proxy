7333c734c6985a74a7cb916d2f37ed624fc1b46f
Robin Howard <robin@cipherstash.com>
2025-02-26 18:33:32 +1100

    feat: Have the AWS RDS certificate bundle be optionally loaded into the system cert store.
    Set the `CS_DATABASE__INSTALL_AWS_RDS_CERT_BUNDLE` env var (eg. to
    `"true"`) to have the bundle be loaded in at container start.
    
    (The certificate is fetched and bundled on build, so as long as the
    proxy image isn't four years old or something, it should be fine.)

---
+++ docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -eu
+
+: "${CS_DATABASE__AWS_BUNDLE_PATH:=./aws-rds-global-bundle.pem}"
+
+# Optionally pull in the AWS RDS global certificate bundle. This is required
+# to communicate with AWS RDS instances, if they are not configured to use some
+# other certificates.
+# (This assumes that ca-certificates is installed, for csplit and update-ca-certificates.)
+case "${CS_DATABASE__INSTALL_AWS_RDS_CERT_BUNDLE:-}" in
+  # Have a guess at some common yaml-et-al encoding failures:
+  "") ;&
+  "false") ;&
+  "no") ;&
+  "0")
+    >&2 echo "Not installing AWS RDS certificate bundle."
+    ;;
+
+  # Okay, go ahead and install the bundle:
+  *)
+    set -x
+    if [ ! -f "$CS_DATABASE__AWS_BUNDLE_PATH" ]; then
+      >&2 echo "Unable to find AWS RDS certificate bundle at: $CS_DATABASE__AWS_BUNDLE_PATH"
+      exit 1
+    fi
+
+    >&2 echo "Installing AWS RDS certificate bundle..."
+    csplit --quiet --elide-empty-files --prefix /usr/local/share/ca-certificates/aws --suffix '.%d.crt' "$CS_DATABASE__AWS_BUNDLE_PATH" '/-----BEGIN CERTIFICATE-----/' '{*}'
+    update-ca-certificates
+    ;;
+esac
+
+exec cipherstash-proxy "$@"
