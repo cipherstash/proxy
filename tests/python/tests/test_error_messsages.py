43bd003c8001e06f6b65b538d10645ee72b97436
Toby Hede <toby@cipherstash.com>
2025-02-27 20:20:55 +1100

    Actually commit python test

--- tests/python/tests/test_error_messsages.py
+++ tests/python/tests/test_error_messsages.py
@@ -45,3 +45,31 @@ def test_encrypted_column_not_defined_in_schema():
                     assert(msg.find('column "encrypted_unconfigured" of relation "encrypted" does not exist') == 0)
 
 
+
+def test_encrypted_column_with_no_configuration():
+    with psycopg.connect(connection_str, autocommit=True) as conn:
+
+        with conn.cursor() as cursor:
+
+            with conn.transaction():
+
+                id = make_id()
+
+                val = '{"hello": "world"}'
+
+                sql = "INSERT INTO unconfigured (id, encrypted_unconfigured) VALUES (%s, %s)"
+
+                try:
+                    cursor.execute(sql, [id, val])
+
+                    # Unreachable
+                    assert(false)
+
+                except Exception as err:
+                    msg = str(err)
+                    assert(msg.find("Column 'encrypted_unconfigured' in table 'unconfigured' has no Encrypt configuration. For help visit https://github.com/cipherstash/proxy/docs/errors.md#encrypt-unknown-column") == 0)
+
+
+
+
+
