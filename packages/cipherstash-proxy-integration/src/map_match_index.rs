7358b3f652df09ec6b7a8a84efdf66cb762f2faf
Toby Hede <toby@cipherstash.com>
2025-03-24 16:01:57 +1100

    chore(deps): update to eql 1.0.1

--- packages/cipherstash-proxy-integration/src/map_match_index.rs
+++ packages/cipherstash-proxy-integration/src/map_match_index.rs
@@ -16,7 +16,17 @@ mod tests {
         let sql = "INSERT INTO encrypted (id, encrypted_text) VALUES ($1, $2)";
         client.query(sql, &[&id, &encrypted_text]).await.unwrap();
 
-        let sql = "SELECT id, encrypted_text FROM encrypted WHERE encrypted_text @> $1";
+        let sql = "SELECT id, encrypted_text FROM encrypted WHERE encrypted_text LIKE $1";
+        let rows = client.query(sql, &[&"hello@"]).await.unwrap();
+
+        assert_eq!(rows.len(), 1);
+
+        for row in rows {
+            let result: String = row.get("encrypted_text");
+            assert_eq!(encrypted_text, result);
+        }
+
+        let sql = "SELECT id, encrypted_text FROM encrypted WHERE encrypted_text ~~ $1";
         let rows = client.query(sql, &[&"hello@"]).await.unwrap();
 
         assert_eq!(rows.len(), 1);
