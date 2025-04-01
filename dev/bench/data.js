7127bcce9997f847ef4b943e1f9a6e682b6f09fb
github-action-benchmark <github@users.noreply.github.com>
2025-04-01 04:39:34 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for f4e14a51e8210516048c1150a078c0b596eeccdc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743481182159,
+  "lastUpdate": 1743482374466,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -24,6 +24,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 6.79663,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "f4e14a51e8210516048c1150a078c0b596eeccdc",
+          "message": "feat: pgbench with postgres, pgbouncer and proxy",
+          "timestamp": "2025-03-31T22:53:28Z",
+          "url": "https://github.com/cipherstash/proxy/pull/197/commits/f4e14a51e8210516048c1150a078c0b596eeccdc"
+        },
+        "date": 1743482372983,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 6.824534,
             "unit": "Number"
           }
         ]
