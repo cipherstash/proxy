914564e7b75cbf10c2a61a77f17f5063682c2f15
James Sadler <james@cipherstash.com>
2025-05-06 16:35:53 +1000

    chore(mapper): replaced literals are now `ROW(..)` expressions

--- packages/eql-mapper/src/transformation_rules/replace_plaintext_eql_literals.rs
+++ packages/eql-mapper/src/transformation_rules/replace_plaintext_eql_literals.rs
@@ -1,6 +1,9 @@
 use std::{any::type_name, collections::HashMap};
 
-use sqltk::parser::ast::Value;
+use sqltk::parser::ast::{
+    CastKind, DataType, Expr, Function, FunctionArg, FunctionArgExpr, FunctionArgumentList,
+    FunctionArguments, Ident, ObjectName, Value,
+};
 use sqltk::{NodeKey, NodePath, Visitable};
 
 use crate::EqlMapperError;
@@ -24,11 +27,12 @@ impl<'ast> TransformationRule<'ast> for ReplacePlaintextEqlLiterals<'ast> {
         node_path: &NodePath<'ast>,
         target_node: &mut N,
     ) -> Result<bool, EqlMapperError> {
+        if let Some((Expr::Value(value),)) = node_path.last_1_as::<Expr>() {
         if self.would_edit(node_path, target_node) {
-            if let Some((value,)) = node_path.last_1_as::<Value>() {
+            if let Some((Expr::Value(value),)) = node_path.last_1_as::<Expr>() {
                 if let Some(replacement) = self.encrypted_literals.remove(&NodeKey::new(value)) {
-                    let target_node = target_node.downcast_mut::<Value>().unwrap();
-                    *target_node = replacement;
+                    let target_node = target_node.downcast_mut::<Expr>().unwrap();
+                    *target_node = make_row_expression(replacement);
                     return Ok(true);
                 }
             }
@@ -38,7 +42,7 @@ impl<'ast> TransformationRule<'ast> for ReplacePlaintextEqlLiterals<'ast> {
     }
 
     fn would_edit<N: Visitable>(&mut self, node_path: &NodePath<'ast>, _target_node: &N) -> bool {
-        if let Some((value,)) = node_path.last_1_as::<Value>() {
+        if let Some((Expr::Value(value),)) = node_path.last_1_as::<Expr>() {
             return self.encrypted_literals.contains_key(&NodeKey::new(value));
         }
         false
@@ -54,4 +58,26 @@ impl<'ast> TransformationRule<'ast> for ReplacePlaintextEqlLiterals<'ast> {
             )))
         }
     }
+}
+
+fn make_row_expression(replacement: Value) -> Expr {
+    Expr::Function(Function {
+        name: ObjectName(vec![Ident::new("ROW")]),
+        uses_odbc_syntax: false,
+        parameters: FunctionArguments::None,
+        args: FunctionArguments::List(FunctionArgumentList {
+            duplicate_treatment: None,
+            clauses: vec![],
+            args: vec![FunctionArg::Unnamed(FunctionArgExpr::Expr(Expr::Cast {
+                kind: CastKind::DoubleColon,
+                expr: Box::new(Expr::Value(replacement)),
+                data_type: DataType::JSONB,
+                format: None,
+            }))],
+        }),
+        filter: None,
+        null_treatment: None,
+        over: None,
+        within_group: vec![],
+    })
 }
