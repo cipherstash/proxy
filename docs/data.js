065a033c71474a4fa7e86a848cbe3af2784ca242
github-action-benchmark <github@users.noreply.github.com>
2025-05-28 07:16:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b187c7d9954457e199612670c853c7325715e0a3

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748416496831,
+  "lastUpdate": 1748416600342,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -2961,6 +2961,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 6.80646,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "b187c7d9954457e199612670c853c7325715e0a3",
+          "message": "Additional test coverage",
+          "timestamp": "2025-05-27T05:48:37Z",
+          "url": "https://github.com/cipherstash/proxy/pull/246/commits/b187c7d9954457e199612670c853c7325715e0a3"
+        },
+        "date": 1748416598886,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 6.813335,
             "unit": "Number"
           }
         ]
