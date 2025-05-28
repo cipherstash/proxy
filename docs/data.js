cccafde14c475fc3e74e6cf009e00ef5a33a305e
github-action-benchmark <github@users.noreply.github.com>
2025-05-28 23:48:46 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3feaea1545c6101d4716bb52940d9680e250a327

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748426341577,
+  "lastUpdate": 1748476126253,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -3111,6 +3111,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 6.74212,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "3feaea1545c6101d4716bb52940d9680e250a327",
+          "message": "Additional test coverage",
+          "timestamp": "2025-05-27T05:48:37Z",
+          "url": "https://github.com/cipherstash/proxy/pull/246/commits/3feaea1545c6101d4716bb52940d9680e250a327"
+        },
+        "date": 1748476124820,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 6.723278,
             "unit": "Number"
           }
         ]
