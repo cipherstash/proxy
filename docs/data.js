ae1c854a55072b884f82a8ca331a38825b9c0b2e
github-action-benchmark <github@users.noreply.github.com>
2025-05-30 03:37:24 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for fcec4856387771a75b397a663ccdef46213ae7db

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748569718417,
+  "lastUpdate": 1748576244466,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -3261,6 +3261,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 7.097729,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "fcec4856387771a75b397a663ccdef46213ae7db",
+          "message": "Additional test coverage for order and group",
+          "timestamp": "2025-05-27T05:48:37Z",
+          "url": "https://github.com/cipherstash/proxy/pull/246/commits/fcec4856387771a75b397a663ccdef46213ae7db"
+        },
+        "date": 1748576243001,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 6.805225,
             "unit": "Number"
           }
         ]
