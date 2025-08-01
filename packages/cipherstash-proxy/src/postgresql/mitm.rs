7622af5a819d415d7247edff76215f6e0b1c7773
James Sadler <james@cipherstash.com>
2025-08-01 22:19:33 +1000

    WIP

--- packages/cipherstash-proxy/src/postgresql/mitm.rs
+++ packages/cipherstash-proxy/src/postgresql/mitm.rs
@@ -2,42 +2,40 @@ use bytes::BytesMut;
 use tokio::io::{ReadHalf, WriteHalf};
 
 use crate::{
-    connect::AsyncStream, error::Error, postgresql::messages::{BackendCode, FrontendCode}
+    connect::AsyncStream,
+    error::Error,
+    postgresql::messages::{BackendCode, FrontendCode},
 };
 
-struct Mitm<FC, FS> {
+struct Mitm {
     client_reader: ReadHalf<AsyncStream>,
     client_writer: WriteHalf<AsyncStream>,
-    client_message_transformer: FC,
+    client_message_transformer: fn(ClientMessage) -> ClientMessage,
     server_reader: ReadHalf<AsyncStream>,
     server_writer: WriteHalf<AsyncStream>,
-    server_message_transformer: FS,
+    server_message_transformer: fn(ServerMessage) -> ServerMessage,
 }
 
 pub(crate) struct ClientMessage(FrontendCode, BytesMut);
 pub(crate) struct ServerMessage(BackendCode, BytesMut);
 
 enum Message {
-    Client(ClientMessage),
-    Server(ServerMessage),
+    ClientMessage(ClientMessage),
+    ServerMessage(ServerMessage),
 }
 
 pub(crate) const NOOP_CLIENT_MESSAGE_TRANSFORMER: fn(ClientMessage) -> ClientMessage =
-    |message: ClientMessage| message;
+    |message| message;
 
 pub(crate) const NOOP_SERVER_MESSAGE_TRANSFORMER: fn(ServerMessage) -> ServerMessage =
-    |message: ServerMessage| message;
+    |message| message;
 
-impl<FC, FS> Mitm<FC, FS>
-where
-    FC: Fn(ClientMessage) -> ClientMessage,
-    FS: Fn(ServerMessage) -> ServerMessage,
-{
+impl Mitm {
     pub(crate) fn new(
         client: AsyncStream,
-        client_message_transformer: FC,
+        client_message_transformer: fn(ClientMessage) -> ClientMessage,
         server: AsyncStream,
-        server_message_transformer: FS,
+        server_message_transformer: fn(ServerMessage) -> ServerMessage,
     ) -> Self {
         let (client_reader, client_writer) = client.split();
         let (server_reader, server_writer) = server.split();
