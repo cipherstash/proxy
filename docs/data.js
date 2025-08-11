55d95c991981e5407614fa5be8b4d31e1b0308fe
github-action-benchmark <github@users.noreply.github.com>
2025-08-11 10:41:19 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3d92d820d366f111a72477502c928d99bb7e0d8e

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754895600542,
+  "lastUpdate": 1754908879651,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -11598,6 +11598,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 7.106319,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "3d92d820d366f111a72477502c928d99bb7e0d8e",
+          "message": "Add SET command for run-time configuration of keyset_id",
+          "timestamp": "2025-08-11T02:14:42Z",
+          "url": "https://github.com/cipherstash/proxy/pull/299/commits/3d92d820d366f111a72477502c928d99bb7e0d8e"
+        },
+        "date": 1754908878252,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 2.605253,
             "unit": "Number"
           }
         ]
