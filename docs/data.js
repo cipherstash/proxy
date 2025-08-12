81fe41e933a97e93a3d0f069ed57148e1db1c281
github-action-benchmark <github@users.noreply.github.com>
2025-08-12 03:19:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3916860dfff90ca64b18e6b377104d178f6a7165

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754967464401,
+  "lastUpdate": 1754968762387,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -11823,6 +11823,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 6.707314,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "3916860dfff90ca64b18e6b377104d178f6a7165",
+          "message": "Adds JSONB tests to the Python integration suite",
+          "timestamp": "2025-08-11T02:14:42Z",
+          "url": "https://github.com/cipherstash/proxy/pull/277/commits/3916860dfff90ca64b18e6b377104d178f6a7165"
+        },
+        "date": 1754968760899,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 6.791322,
             "unit": "Number"
           }
         ]
