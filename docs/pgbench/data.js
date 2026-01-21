14c0e3b275bed432dea6a3b3b76ffb3fb5f6847c
github-action-benchmark <github@users.noreply.github.com>
2026-01-21 06:41:55 +0000

    add pgbench (customBiggerIsBetter) benchmark result for 04e9acffdb5dd3ea1f8bb54be7696c12313f6620

--- docs/pgbench/data.js
+++ docs/pgbench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768977377975,
+  "lastUpdate": 1768977715614,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "pgbench": [
@@ -24,6 +24,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 22.985442,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "04e9acffdb5dd3ea1f8bb54be7696c12313f6620",
+          "message": "feat(benchmark): add k6 benchmarks for JSONB/encrypted query performance",
+          "timestamp": "2026-01-20T03:20:49Z",
+          "url": "https://github.com/cipherstash/proxy/pull/352/commits/04e9acffdb5dd3ea1f8bb54be7696c12313f6620"
+        },
+        "date": 1768977714962,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 24.105574,
             "unit": "Number"
           }
         ]
