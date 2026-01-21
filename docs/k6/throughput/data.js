107b7c12385aaafc2a0f42d86a17cd59dc7dd916
github-action-benchmark <github@users.noreply.github.com>
2026-01-21 05:08:46 +0000

    add k6 Throughput (customBiggerIsBetter) benchmark result for 72128361181f911a8f1e27b3108a32277be1dc69

--- docs/k6/throughput/data.js
+++ docs/k6/throughput/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768971949118,
+  "lastUpdate": 1768972126604,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "k6 Throughput": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "jsonb-ste-vec-insert_rate",
             "value": 92.2,
+            "unit": "iter/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "72128361181f911a8f1e27b3108a32277be1dc69",
+          "message": "feat(benchmark): add k6 benchmarks for JSONB/encrypted query performance",
+          "timestamp": "2026-01-20T03:20:49Z",
+          "url": "https://github.com/cipherstash/proxy/pull/352/commits/72128361181f911a8f1e27b3108a32277be1dc69"
+        },
+        "date": 1768972125223,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "jsonb-large-payload_rate",
+            "value": 86.41,
+            "unit": "iter/s"
+          },
+          {
+            "name": "jsonb-ste-vec-insert_rate",
+            "value": 93.44,
             "unit": "iter/s"
           }
         ]
