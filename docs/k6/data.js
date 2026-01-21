72599c4f637936665926e542be9c81c1b4a8184a
github-action-benchmark <github@users.noreply.github.com>
2026-01-21 02:55:17 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f41215e00517ff4515f1698aeb43de78f9b5d051

---
+++ docs/k6/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1768964117189,
+  "repoUrl": "https://github.com/cipherstash/proxy",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "f41215e00517ff4515f1698aeb43de78f9b5d051",
+          "message": "Ks benchmarks",
+          "timestamp": "2026-01-20T03:20:49Z",
+          "url": "https://github.com/cipherstash/proxy/pull/352/commits/f41215e00517ff4515f1698aeb43de78f9b5d051"
+        },
+        "date": 1768964115568,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "jsonb-insert_iterations_per_second",
+            "value": 10915.19,
+            "unit": "Number"
+          },
+          {
+            "name": "jsonb-insert_p95_ms",
+            "value": 4.36,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
