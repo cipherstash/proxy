5877154e9abc6ce1b9d571b1528af4f049f8f343
github-action-benchmark <github@users.noreply.github.com>
2026-01-21 03:51:05 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5bfa6049ebe14f9fb12056949b9c148b2f09583c

--- docs/k6/data.js
+++ docs/k6/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768964117189,
+  "lastUpdate": 1768967465738,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "jsonb-insert_p95_ms",
             "value": 4.36,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "5bfa6049ebe14f9fb12056949b9c148b2f09583c",
+          "message": "feat(benchmark): add k6 benchmarks for JSONB/encrypted query performance",
+          "timestamp": "2026-01-20T03:20:49Z",
+          "url": "https://github.com/cipherstash/proxy/pull/352/commits/5bfa6049ebe14f9fb12056949b9c148b2f09583c"
+        },
+        "date": 1768967464423,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "jsonb-insert_iterations_per_second",
+            "value": 93.65,
+            "unit": "Number"
+          },
+          {
+            "name": "jsonb-insert_p95_ms",
+            "value": 123.95,
             "unit": "ms"
           }
         ]
