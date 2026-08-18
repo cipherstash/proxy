99ce38fafcf8eae3655b03a99ecedc510483172a
github-action-benchmark <github@users.noreply.github.com>
2026-08-18 06:37:22 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for af008d7bb098606de905501da2a156dff2c47a95

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787034811562,
+  "lastUpdate": 1787035042165,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -24760,6 +24760,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 40.532759,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "af008d7bb098606de905501da2a156dff2c47a95",
+          "message": "test: add proxy burn-in and CRUD benchmark",
+          "timestamp": "2026-08-18T05:20:11Z",
+          "url": "https://github.com/cipherstash/proxy/pull/455/commits/af008d7bb098606de905501da2a156dff2c47a95"
+        },
+        "date": 1787035040329,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 39.644459,
             "unit": "Number"
           }
         ]
