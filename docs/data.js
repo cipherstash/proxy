c6e321e39b03f603149f455f79bb88265d029563
github-action-benchmark <github@users.noreply.github.com>
2026-08-18 04:28:09 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 6fb16418bdd6acd8e21c824f845d39fcb42d5f86

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787020479703,
+  "lastUpdate": 1787027289542,
   "repoUrl": "https://github.com/cipherstash/proxy",
   "entries": {
     "Benchmark": [
@@ -24681,6 +24681,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "tps",
             "value": 40.757352,
+            "unit": "Number"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "committer": {
+            "name": "cipherstash",
+            "username": "cipherstash"
+          },
+          "id": "6fb16418bdd6acd8e21c824f845d39fcb42d5f86",
+          "message": "fix(deps): bump cipherstash-client, cipherstash-config, and cts-common to 0.42.2",
+          "timestamp": "2026-08-18T02:27:36Z",
+          "url": "https://github.com/cipherstash/proxy/pull/456/commits/6fb16418bdd6acd8e21c824f845d39fcb42d5f86"
+        },
+        "date": 1787027287759,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "tps",
+            "value": 40.118414,
             "unit": "Number"
           }
         ]
